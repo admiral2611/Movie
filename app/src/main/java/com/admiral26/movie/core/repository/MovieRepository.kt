19bb036b9f0ac0acd2d5ad7d5adbc8a6fc3b326b
admiral <azizbekmm7@gmail.com>
2024-02-10 15:21:06 +0500

    finish 2

--- app/src/main/java/com/admiral26/movie/core/repository/MovieRepository.kt
+++ app/src/main/java/com/admiral26/movie/core/repository/MovieRepository.kt
@@ -1,38 +1,25 @@
 package com.admiral26.movie.core.repository
 
-import android.util.Log
 import com.admiral26.movie.core.model.footerRes.FooterRespons
 import com.admiral26.movie.core.model.headerRes.HeaderRespons
 import com.admiral26.movie.core.network.ApiClient
 import com.admiral26.movie.core.util.ResultWrapper
 import com.admiral26.movie.core.util.parseData
 import kotlinx.coroutines.Dispatchers
-import java.io.IOException
 
 class MovieRepository {
 
     private val service = ApiClient.createService()
 
-   suspend fun getMoviesHed(): ResultWrapper<HeaderRespons, Any> {
-       return parseData(Dispatchers.IO) {
-           service.getHeader("10137bab07a7a987b23a902a78d6986c")
-       }
-   }
+    suspend fun getMoviesHed(): ResultWrapper<HeaderRespons, Any> {
+        return parseData(Dispatchers.IO) {
+            service.getHeader("10137bab07a7a987b23a902a78d6986c")
+        }
+    }
 
-    suspend fun getMoviesFoot():ResultWrapper<FooterRespons,Any>{
-        return try {
-
-            val result = service.getFooter("10137bab07a7a987b23a902a78d6986c")
-            if (result.isSuccessful){
-                Log.d("bodyFoot", "getMoviesFoot: ${result.body().toString()}")
-                ResultWrapper.Success(result.body(),result.code())
-            }else{
-                ResultWrapper.Error(result.message(),result.code())
-            }
-        }catch (e: IOException) {
-            ResultWrapper.Error("Internet Yoq")
-        } catch (e: Exception) {
-            ResultWrapper.Error(e.message)
+    suspend fun getMoviesFoot(): ResultWrapper<FooterRespons, Any> {
+        return parseData(Dispatchers.IO) {
+            service.getFooter("10137bab07a7a987b23a902a78d6986c")
         }
     }
 }
